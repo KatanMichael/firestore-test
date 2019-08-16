8fb70f1fb7f8aa0c9786407c85fb9a7c408c5dfa
Michael Katan <michaelkatan@gmail.com>
2019-08-16 09:08:33 +0300

    cors problem

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,7 +6,7 @@ function addUserToDB(inputName,inputAge)
     var url = "https://michael-firestorer-test.herokuapp.com/addRandomUser"
 
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, false ); // false for synchronous request
+    xmlHttp.open( "GET", url, true ); // false for synchronous request
     xmlHttp.send( null );
     return xmlHttp.responseText;
 
