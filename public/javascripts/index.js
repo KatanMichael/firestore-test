41586e43223c138be29f755e973195182a0c59d0
Michael Katan <michaelkatan@gmail.com>
2019-08-16 10:42:07 +0300

    ..

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -6,7 +6,7 @@ function addUserToDB(inputName,inputAge)
     var url = "https://michael-firestorer-test.herokuapp.com//fights"
 
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", url, true ); // false for synchronous request
+    xmlHttp.open( "POST", url, true ); // false for synchronous request
     xmlHttp.send( null );
     return xmlHttp.responseText;
 
